b7893a08739b90ab2e63361109c9e193f0d5e299
yourusername <kunay69@hotmail.it>
2022-01-30 20:05:28 +0100

    album page done

--- M3-D5/Spotify-api-project/album.js
+++ M3-D5/Spotify-api-project/album.js
@@ -23,10 +23,75 @@ const mouseoverSong = function () {
 mouseoverSong()
 
 const id = new URLSearchParams(window.location.search).get('albumId')
-
 fetch('https://striveschool-api.herokuapp.com/api/deezer/album/' + id)
   .then(response => response.json())
   .then(data => {
     console.log(data)
+
+    function time(duration) {
+      // Hours, minutes and seconds
+      var hrs = ~~(duration / 3600)
+      var mins = ~~((duration % 3600) / 60)
+      var secs = ~~duration % 60
+      // Output like "1:01" or "4:03:59" or "123:03:59"
+      var ret = ''
+      if (hrs > 0) {
+        ret += '' + hrs + ':' + (mins < 10 ? '0' : '')
+      }
+      ret += '' + mins + ':' + (secs < 10 ? '0' : '')
+      ret += '' + secs
+      return ret
+    }
+
+    const artistNameContainer = document.querySelector('.artist-name-container')
+    const songsList = document.querySelector('.songs-list')
+
+    artistNameContainer.innerHTML = `
+                  <img class="img-fluid d-inline-flex" width="200px" src="${
+                    data.artist.picture_medium
+                  }" alt="${data.title}">
+                  <div class="ml-3">
+                    <h6 class="albumWord">ALBUM</h6>
+                    <h2 class="albumTitle ">${data.title}</h2>
+                    <h6>
+                      <img class="rounded-circle img-fluid bandLogo" src="${
+                        data.artist.picture_medium
+                      }" alt="${data.title}">
+                      <span class="text-muted infoAlbum"> <a href="./artist.html?artistId=${
+                        data.artist.id
+                      }"><span class="text-white">${
+      data.artist.name
+    }</span></a> · ${data.release_date.slice(0, 4)} · ${
+      data.tracks.data.length
+    } tracks · ${time(data.duration)}</span>
+                    </h6>
+                  </div>
+          `
+    data.tracks.data.map((track, i) => {
+      songsList.innerHTML += `
+            <div class="pl-2 song d-flex justify-content-between">
+              <div class="d-flex">
+                <span class="listNumber">${i + 1}</span>
+                <p class="pl-3 text-white">
+                  ${track.title}<br />
+                  <span class="text-muted text-white"><a href="./artist.html?artistId=${
+                    data.artist.id
+                  }" class="text-muted text-white">${
+        data.artist.name
+      }</a></span>
+                </p>
+              </div>
+              <div>
+                <i class="bi bi-heart hidden heartSong"></i>
+                <span class="text-muted px-3">${time(track.duration)}</span>
+                <i class="bi bi-three-dots hidden threeDotsSong"></i>
+              </div>
+            </div>
+    `
+    })
   })
   .catch(e => console.log(e))
+
+// const header = `<h1>${data.title}</h1>
+//                          <a href="./artist.html?artistId=${data.artist.id}"> <h5>${data.artist.name}</h5></a>
+//         `;
