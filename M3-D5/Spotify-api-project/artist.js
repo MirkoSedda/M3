cbedd96a79cacf80ad710e271c4f0bc5838eafb1
yourusername <kunay69@hotmail.it>
2022-01-30 21:48:21 +0100

    need to fix blackground on artist page

--- M3-D5/Spotify-api-project/artist.js
+++ M3-D5/Spotify-api-project/artist.js
@@ -22,17 +22,83 @@ const mouseoverSong = function () {
 }
 mouseoverSong()
 
-const expander = function(){
-  const seeMoreNode = document.querySelector(".seeMore")
-  const hiddenSongNode = document.getElementsByClassName("hiddenSong")
+const id = new URLSearchParams(window.location.search).get('artistId')
+fetch('https://striveschool-api.herokuapp.com/api/deezer/artist/' + id)
+  .then(response => response.json())
+  .then(
+    (_artistData = artistData => {
+      console.log(artistData)
 
-  seeMoreNode.addEventListener("click",function(e){
-    for(let i =0;i<hiddenSongNode.length;i++){
-      hiddenSongNode[i].classList.remove("hiddenSong")
+      const artistNameContainer = document.querySelector(
+        '.artist-name-container'
+      )
+      artistNameContainer.style.backgroundImage = `url(${artistData.picture_big})`
+
+      const numberWithCommas = x =>
+        x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+
+      artistNameContainer.innerHTML = `
+      <div class="position-absolute artist-name">
+      <p class="mb-0">
+      <i class="bi bi-patch-check-fill pr-2"></i>Verified Artist
+      </p>
+      <h1>${artistData.name}</h1>
+      <p>${numberWithCommas(artistData.nb_fan)} monthly listeners</p>
+      </div>
+      
+      `
+    })
+  )
+
+fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem`)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+    const songsContainer = document.querySelector('.songsContainer')
+    const songs = data.data
+    function time(duration) {
+      // Hours, minutes and seconds
+      var hrs = ~~(duration / 3600)
+      var mins = ~~((duration % 3600) / 60)
+      var secs = ~~duration % 60
+      // Output like "1:01" or "4:03:59" or "123:03:59"
+      var ret = ''
+      if (hrs > 0) {
+        ret += '' + hrs + ':' + (mins < 10 ? '0' : '')
+      }
+      ret += '' + mins + ':' + (secs < 10 ? '0' : '')
+      ret += '' + secs
+      return ret
     }
 
-  })
-}
-expander()
+    const numberWithCommas = () =>
+      Math.floor(Math.random() * 10000000)
+        .toString()
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
 
-// could be greater if it creates 5 songs instead unhide 5
+    songs.map((song, i) => {
+      console.log(song.album)
+      songsContainer.innerHTML += `
+        <div class="row song mt-3">
+          <div class="col-md-6 d-flex align-items-center">
+            <span class="listNumber text-muted">${i + 1}</span>
+            <img src="${song.album.cover_small}" alt="" class="ml-2" />
+            <p class="pl-3 m-0 text-white">
+              ${song.album.title} <br />
+            </p>
+          </div>
+          <div class="col-md-3 d-flex justify-content-center align-items-center">
+            <div>
+              <span class="text-muted text-right">${numberWithCommas()}</span>
+            </div>
+          </div>
+          <div class="col-md-3 d-flex align-items-center">
+            <i class="bi bi-heart hidden heartSong pr-1"></i>
+            <span class="text-muted time px-3">${time(song.duration)}</span>
+            <i class="bi bi-three-dots hidden threeDotsSong text-muted"></i>
+          </div>
+        </div>      
+          `
+    })
+  })
+  .catch(e => console.log(e))
