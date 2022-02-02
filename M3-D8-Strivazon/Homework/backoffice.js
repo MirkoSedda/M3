3401b6ab6a90e3f757c67c1ba8211652457bdf45
yourusername <kunay69@hotmail.it>
2022-02-02 19:23:51 +0100

    M3-D8-done

--- M3-D8-Strivazon/Homework/backoffice.js
+++ M3-D8-Strivazon/Homework/backoffice.js
@@ -25,4 +25,5 @@ const handleSubmit = event => {
         'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWZhNjY0ZTgyZWExZDAwMTViYjAzZWMiLCJpYXQiOjE2NDM4MDAxNDIsImV4cCI6MTY0NTAwOTc0Mn0.P9w7OXz4zve7wfC83LXdJteK6TsglIMohh94l3bqIiA',
     },
   })
+  window.location.replace('/Homework/homepage.html')
 }
