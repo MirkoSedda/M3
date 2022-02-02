7f39643f6f8428f3b0de595776df4436e62719ac
yourusername <kunay69@hotmail.it>
2022-02-02 17:53:38 +0100

    M3-D8 done

--- M3-D8-Strivazon/Homework/backoffice.js
+++ M3-D8-Strivazon/Homework/backoffice.js
@@ -1,10 +1,28 @@
-// name
-// descriptions
-// brand
-// price
-fetch('https://striveschool-api.herokuapp.com/api/product/', {
-  headers: {
-    Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWZhNjY0ZTgyZWExZDAwMTViYjAzZWMiLCJpYXQiOjE2NDM4MDAxNDIsImV4cCI6MTY0NTAwOTc0Mn0.P9w7OXz4zve7wfC83LXdJteK6TsglIMohh94l3bqIiA',
-  },
-})
+const handleSubmit = event => {
+  event.preventDefault()
+
+  const name = document.getElementById('name').value
+  const description = document.getElementById('description').value
+  const brand = document.getElementById('brand').value
+  const imageUrl = document.getElementById('imageUrl').value
+  const price = document.getElementById('price').value
+
+  const payload = {
+    name,
+    description,
+    brand,
+    imageUrl,
+    price,
+  }
+
+  console.log(payload)
+  fetch('https://striveschool-api.herokuapp.com/api/product/', {
+    method: 'POST',
+    body: JSON.stringify(payload),
+    headers: {
+      'content-type': 'application/json',
+      authorization:
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWZhNjY0ZTgyZWExZDAwMTViYjAzZWMiLCJpYXQiOjE2NDM4MDAxNDIsImV4cCI6MTY0NTAwOTc0Mn0.P9w7OXz4zve7wfC83LXdJteK6TsglIMohh94l3bqIiA',
+    },
+  })
+}
--- M3-D8-Strivazon/Homework/homepage.js
+++ M3-D8-Strivazon/Homework/homepage.js
@@ -1,6 +1,28 @@
-fetch('https://striveschool-api.herokuapp.com/api/product/', {
-  headers: {
-    Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWZhNjY0ZTgyZWExZDAwMTViYjAzZWMiLCJpYXQiOjE2NDM4MDAxNDIsImV4cCI6MTY0NTAwOTc0Mn0.P9w7OXz4zve7wfC83LXdJteK6TsglIMohh94l3bqIiA',
-  },
-})
+const getData = () => {
+  fetch('https://striveschool-api.herokuapp.com/api/product/', {
+    headers: {
+      'content-type': 'application/json',
+      authorization:
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWZhNjY0ZTgyZWExZDAwMTViYjAzZWMiLCJpYXQiOjE2NDM4MDAxNDIsImV4cCI6MTY0NTAwOTc0Mn0.P9w7OXz4zve7wfC83LXdJteK6TsglIMohh94l3bqIiA',
+    },
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+
+      data.map(x => {
+        const app = document.querySelector('.app')
+
+        app.innerHTML += `<ul class='list-group'>
+      <li class="list-group-item"><b>Name: </b> ${x.name}</li>
+      <li class="list-group-item"><b>Description: </b> ${x.description}</li>
+      <li class="list-group-item"><b>Brand: </b> ${x.brand}</li>
+      <li class="list-group-item"><b>Price: </b> ${x.price}</li>
+    </ul>`
+      })
+    })
+}
+
+window.onload = () => {
+  getData()
+}
