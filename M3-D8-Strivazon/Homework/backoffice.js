6e13e5a7266ad8fa7135e75618b2807d52a8e6b4
yourusername <kunay69@hotmail.it>
2022-02-02 12:13:40 +0100

    api set

--- M3-D8-Strivazon/Homework/backoffice.js
+++ M3-D8-Strivazon/Homework/backoffice.js
@@ -1,1 +1,10 @@
-alert('hello')
+// name
+// descriptions
+// brand
+// price
+fetch('https://striveschool-api.herokuapp.com/api/product/', {
+  headers: {
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWZhNjY0ZTgyZWExZDAwMTViYjAzZWMiLCJpYXQiOjE2NDM4MDAxNDIsImV4cCI6MTY0NTAwOTc0Mn0.P9w7OXz4zve7wfC83LXdJteK6TsglIMohh94l3bqIiA',
+  },
+})
